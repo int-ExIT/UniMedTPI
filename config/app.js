cd1a3718cb17a44bd1280d9bc0408c196a238a39
int-ExIT <eittien@gmail.com>
2025-06-18 01:54:05 -0300

    .....

--- config/app.js
+++ config/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const config = require("../config");
 const morgan = require("morgan");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,5 +15,8 @@ app.use("/public", express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors({
+  origin: 'https://unimedtpi-production.up.railway.app'
+}));
 
 module.exports = app;
